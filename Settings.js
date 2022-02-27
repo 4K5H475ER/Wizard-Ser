7a3049d158f9daecd6ee7a9a59de6c8f7445426b
4K5H475ER <96863573+4K5H475ER@users.noreply.github.com>
2022-02-27 11:28:30 +0530

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -1,2 +1,31 @@
 /*COMMING SOON
 YOU CAN CHANGE OWNER NUMBER BOT NAME AND OWNER NAME HERE*/
+{
+
+     "ownerNumber": "12266460887@s.whatsapp.net",
+
+     "fake": "ʀⷪᴏᷫᴅᷫɢͥᴇͨʀͥᴀᷧʟᷝғᴀ",
+
+     "prefix": ".",
+
+     "ownerName": "ʀⷪᴏᷫᴅᷫɢͥᴇͨʀͥᴀᷧʟᷝғᴀ",
+
+     "gamewaktu": 60,
+
+     "owner": "12266460887",
+
+     "botName": "Rodger Bot",
+
+     "lolkey": "2c5a33509a07ebf7920b9a08",
+
+     "vhtearkey": "FauzanBotz",
+
+     "zekskey": "hehe",
+
+     "xteamkey": "heh",
+
+     "txtDonasi": "GoPay : 0821304\nSaweeria : https://saweria.co/hehe hehe\nDana : 088421519",
+
+     "gcount": 20
+
+}
